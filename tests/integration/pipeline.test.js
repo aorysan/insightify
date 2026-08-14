40c7a5ab70d0589ef48698d960115f5974cab499
Aryo Adi Putro <aryoadiputro@gmail.com>
2026-08-14 15:14:00 +0700

    test: scaffold testing directory and fixtures

---
+++ tests/integration/pipeline.test.js
@@ -0,0 +1,7 @@
+const test = require('node:test');
+const assert = require('node:assert');
+
+test('Integration: Pipeline sanity check', (t) => {
+  // TODO: Add actual end-to-end orchestration tests matching scenarios defined in spec
+  assert.strictEqual(1, 1, 'Basic test setup works');
+});
